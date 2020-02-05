56f2e6eeddd3ca57c8b0f8cba2bc86905f4073ec
Dhruv <dhruvpatel5738@gmail.com>
2020-02-04 18:45:42 -0800

    Added aiming

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 var io = require('socket.io')(process.env.PORT || 7777)
 
 var Player = require('./Classes/Player.js')
+var Bullet = require('./Classes/Bullet.js')
 
 console.log('Server has started')
 
 var players = []
 var sockets = []
+var bullets = []
+
+//Updates
+setInterval(() => {
+    bullets.forEach(bullet => {
+        var isDestroyed = bullet.onUpdate()
+
+        if(isDestroyed){
+            var index = bullets.indexOf(bullet)
+            if(index > -1){
+                bullets.splice(index, 1)
+
+                var returnData = {
+                    "id": bullet.id
+                }
+
+                for(var playerID in players){
+                    sockets[playerID].emit("serverUnspawn", returnData)
+                }
+
+            }
+        } else {
+            var returnData = {
+                "id": bullet.id,
+                "position": {
+                    "x": bullet.position.x,
+                    "y": bullet.position.y
+                }
+            }
+
+            for (var playerID in players) {
+                sockets[playerID].emit("updatePosition", returnData)
+            }
+        }
+    })
+}, 100, 0);
 
 io.on('connection', function(socket) {
     console.log("Connection made")
@@ -44,6 +81,32 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('updateRotation', player)
     })
 
+    socket.on("fireBullet", function(data){
+        var bullet = new Bullet();
+        bullet.name = "Bullet";
+        bullet.position.x = data.position.x
+        bullet.position.y = data.position.y
+        bullet.direction.x = data.direction.x
+        bullet.direction.y = data.direction.y
+
+        bullets.push(bullet)
+
+        var returnData = {
+            "name": bullet.name,
+            "id": bullet.id,
+            "position": {
+                "x": bullet.position.x,
+                "y": bullet.position.y
+            },
+            "direction": {
+                "x": bullet.direction.x,
+                "y": bullet.direction.y
+            }
+        }
+        socket.emit('serverSpawn', returnData)
+        socket.broadcast.emit('serverSpawn', returnData)
+    })
+
     socket.on('disconnect', function(){
         console.log("A player has disconnected")
         delete players[thisPlayerID]
@@ -51,3 +114,21 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('disconnected', player)
     })
 })
+
+function interval(func, wait, time){
+    var interv = function(w, t){
+        return function(){
+            if(typeof t === "undefined" || t-- > 0){
+                setTimeout(interv, w);
+                try {
+                    func.call(null);
+                } catch(e){
+                    t = 0;
+                    throw e.toString();
+                }
+            }
+        }
+    }(wait, time);
+
+    setTimeout(interv, wait);
+}
